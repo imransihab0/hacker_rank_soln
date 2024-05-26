2ee52beeebb6bfb201b5ae7eb9492fbaf0a7176d
Imranul Islam Shihab <imran.sihab.0@gmail.com>
2024-05-26 23:09:25 +0600

    at [ 19/May/2024 ]

---
+++ Maximum_Draws.cpp
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/maximum-draws/problem?isFullScreen=true
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define FastIO() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << ++n << endl;
+}
+
+int32_t main(){
+    FastIO();
+    
+    int t;
+    cin >> t;
+    while(t--) solve();
+    
+return 0;
+}
