26dacb32dfda5fbda7c59e25bc6cb0c1b7491319
Imranul Islam Shihab <imran.sihab.0@gmail.com>
2024-05-28 15:07:22 +0600

    at [ 28/May/2024 ]

---
+++ Cutting_Paper_Squares.cpp
@@ -0,0 +1,19 @@
+// https://hackerrank.com/challenges/p1-paper-cutting/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define FastIO() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int32_t main(){
+    FastIO();
+    
+    int n, m;
+    cin >> n >> m;
+    cout << n*m-1 << endl;
+    
+return 0;
+}
